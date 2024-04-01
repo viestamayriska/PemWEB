17f3ca3192e47ef7f0095d8cf9419d656e64f339
viestamayriska <vristamayriska@gmail.com>
2024-04-01 20:27:53 +0700

    pertemuan6

---
+++ pertemuan6/praktik_jquery/jqueryui.java
@@ -0,0 +1,16 @@
+<!DOCTPYE html>
+<html>
+<head>
+<link rel="stylesheet" href="jquery-ui-1.13.2/jquery-ui.css">
+<script src="jquery-3.5.1.js"></script>
+<script src="jquery-ui-1.13.2/jquery-ui.js"></script>
+<script>
+$(function(){
+    $("#date_ex").datepicker();
+});
+</script>
+<body>
+<div> Select Date: <input type="text" id="date_ex"></div>
+</body>
+</html>
+
